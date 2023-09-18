43aa3c8fd44e19ac8ec7eff91869ba3d5a97e209
CasperWychgel <casper.wychgel.github@gmail.com>
2023-09-18 13:21:56 +0200

    Revert "Authentication and routes updated"
    This reverts commit 901634ff20cee54c615a3105dadee12d9c9d2637.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,17 +8,11 @@ use App\Http\Controllers\HomeController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::resources([
-    'projects' => ProjectController::class,
-    'users' => UserController::class,
-]);
-
-Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\ProjectController::class, 'admin'])->name('admin');
+Route::get('/', function () {
+    return view('welcome');
+});
