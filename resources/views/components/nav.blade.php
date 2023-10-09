5add1ac2384be121fc052e87c08df539002ccd5f
CasperWychgel <casper.wychgel.github@gmail.com>
2023-10-09 15:28:54 +0200

    Updated nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,84 +1,74 @@
-<nav
-    class="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-4
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-        "
->
-    <div>
-        <a href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-    </div>
-
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        id="menu-button"
-        class="h-6 w-6 cursor-pointer md:hidden block"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-    >
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-        />
-    </svg>
-
-    <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-        <ul
-            class="
-              pt-4
-              text-base text-gray-700
-              md:flex
-              md:justify-between
-              md:pt-0"
-        >
-            <li>
-                <a class="md:p-4 py-2 block hover:text-purple-400" href="{{route('projects.index')}}"
-                >Projects</a
-                >
-            </li>
-            <!-- Authentication Links -->
-            @guest
-                @if (Route::has('login'))
-                    <li class="">
-                        <a class="md:p-4 py-2 block hover:text-purple-400" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @endif
-
-                @if (Route::has('register'))
-                    <li class="">
-                        <a class="md:p-4 py-2 block hover:text-purple-400" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="">
-                    <a id="navbarDropdown" class="md:p-4 py-2 block hover:text-purple-400" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
+<nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+            <div class="flex items-center">
+                <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                        <a href="{{ url('/') }}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                           aria-current="page">Dashboard</a>
+                        <a href="{{route('projects.index')}}"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                        <a href="#"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Community</a>
                     </div>
-                </li>
+                </div>
+            </div>
+            @guest
+                <div class="">
+                    <div class="ml-4 flex items-center md:ml-6">
+                        <!-- Profile dropdown -->
+                        <div class="relative ml-3">
+                            <div
+                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                                tabindex="-1">
+                                @if (Route::has('login'))
+                                    <a class="block px-4 py-2 text-sm text-gray-700"
+                                       href="{{ route('login') }}">{{ __('Login') }}</a>
+                                @endif
+                                @if (Route::has('register'))
+                                    <a class="block px-4 py-2 text-sm text-gray-700"
+                                       href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @else
+                <div class="">
+                    <div class="ml-4 flex items-center md:ml-6">
+                        <!-- Profile dropdown -->
+                        <div class="relative ml-3">
+                            <div>
+                                <button type="button"
+                                        class="relative text-white flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                        id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }}
+                                </button>
+                            </div>
+                            <div
+                                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                                tabindex="-1">
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                                   id="user-menu-item-0">Your Profile</a>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                                   id="user-menu-item-1">Settings</a>
+                                <a class="block px-4 py-2 text-sm text-gray-700" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endguest
-        </ul>
+
+        </div>
     </div>
 </nav>
