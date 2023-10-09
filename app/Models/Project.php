e3a8244fedbb91adfd903187867e0efc242e7bc2
CasperWychgel <casper.wychgel.github@gmail.com>
2023-10-09 15:28:45 +0200

    Added images model and migrations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,4 +14,8 @@ class Project extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
 }
